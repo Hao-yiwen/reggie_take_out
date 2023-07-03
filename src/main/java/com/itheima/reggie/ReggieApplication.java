fa3415ec3e403f3492aa149d648d37d6ea624749
郝宜文 <1552975589@qq.com>
2023-07-03 23:17:58 +0800

    feat: 员工功能完善

--- src/main/java/com/itheima/reggie/ReggieApplication.java
+++ src/main/java/com/itheima/reggie/ReggieApplication.java
@@ -9,6 +9,7 @@ import org.springframework.boot.web.servlet.ServletComponentScan;
 @SpringBootApplication
 @ServletComponentScan
 public class ReggieApplication {
+//    TODO: spring boot project startUp
     public static void main(String[] args) {
         SpringApplication.run(ReggieApplication.class, args);
         log.info("项目启动成功~");
